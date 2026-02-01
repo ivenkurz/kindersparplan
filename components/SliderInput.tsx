516a7b34d612ebe046038106ca1b160df6870b6d
ivenkurz <ik@evergreen.de>
2026-02-01 10:50:11 +0100

    UI: Slider-Ticks sichtbar + Thumb ohne Sprung

--- components/SliderInput.tsx
+++ components/SliderInput.tsx
@@ -84,17 +84,6 @@ export default function SliderInput({
               {thumbLabel}
             </div>
           )}
-          {hasSnapTicks && (
-            <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 px-3 flex justify-between pointer-events-none">
-              {snapTicksForRender!.map((v) => (
-                <span
-                  key={v}
-                  className="w-px h-2 bg-white/90"
-                  aria-hidden="true"
-                />
-              ))}
-            </div>
-          )}
           <input
             id={id}
             type="range"
@@ -103,7 +92,7 @@ export default function SliderInput({
             step={step}
             value={value}
             onChange={(e) => onChange(Number(e.target.value))}
-            className="absolute inset-x-0 top-1/2 -translate-y-1/2 w-full h-4 md:h-2.5 bg-transparent appearance-none cursor-pointer
+            className="absolute inset-x-0 top-1/2 -translate-y-1/2 z-10 w-full h-4 md:h-2.5 bg-transparent appearance-none cursor-pointer
               [&::-webkit-slider-runnable-track]:h-full [&::-webkit-slider-runnable-track]:bg-ds-neutral-20 [&::-webkit-slider-runnable-track]:rounded-full
               [&::-moz-range-track]:h-full [&::-moz-range-track]:bg-ds-neutral-20 [&::-moz-range-track]:rounded-full
               [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-7 [&::-webkit-slider-thumb]:h-7 md:[&::-webkit-slider-thumb]:w-5 md:[&::-webkit-slider-thumb]:h-5
@@ -111,13 +100,19 @@ export default function SliderInput({
               [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-ds-orange-60
               [&::-webkit-slider-thumb]:shadow-md [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:border-0
               [&::-webkit-slider-thumb]:transition-transform [&::-webkit-slider-thumb]:transition-shadow [&::-webkit-slider-thumb]:duration-150
-              active:[&::-webkit-slider-thumb]:scale-110 active:[&::-webkit-slider-thumb]:shadow-ds-orange-glow
-              [&:hover::-webkit-slider-thumb]:shadow-ds-orange-glow
+              active:[&::-webkit-slider-thumb]:shadow-ds-orange-glow [&:hover::-webkit-slider-thumb]:shadow-ds-orange-glow
               [&::-moz-range-thumb]:w-7 [&::-moz-range-thumb]:h-7 md:[&::-moz-range-thumb]:w-5 md:[&::-moz-range-thumb]:h-5
               [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-ds-orange-60 [&::-moz-range-thumb]:border-0
               [&::-moz-range-thumb]:transition-transform [&::-moz-range-thumb]:duration-150
-              active:[&::-moz-range-thumb]:scale-110"
+              active:[&::-moz-range-thumb]:shadow-ds-orange-glow"
           />
+          {hasSnapTicks && (
+            <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 z-20 px-3 flex justify-between pointer-events-none">
+              {snapTicksForRender!.map((v) => (
+                <span key={v} className="w-px h-2 bg-white/90" aria-hidden="true" />
+              ))}
+            </div>
+          )}
         </div>
         {rightLabel && (
           <span className="text-xs text-ds-neutral-70 whitespace-nowrap shrink-0">
