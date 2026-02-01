7a626dba5c119ee5cd6985779d3eef369ae06025
ivenkurz <ik@evergreen.de>
2026-02-01 11:32:10 +0100

    Chart: Legende nutzt Linienfarben (Einzahlungen/Ertrag)

--- components/ValueChart.tsx
+++ components/ValueChart.tsx
@@ -364,11 +364,11 @@ export default function ValueChart({ data, view = "spanne", fill = false }: Valu
           <>
             <div className="flex flex-wrap items-center justify-center gap-4 text-xs font-semibold text-ds-neutral-90">
               <div className="flex items-center gap-2">
-                <span className="w-3 h-3 rounded-sm bg-[#616a65]/25 border border-ds-neutral-20" />
+                <span className="w-4 h-[3px] rounded-full bg-[#616a65]" />
                 Einzahlungen
               </div>
               <div className="flex items-center gap-2">
-                <span className="w-3 h-3 rounded-sm bg-[#008542]/20 border border-ds-neutral-20" />
+                <span className="w-4 h-[3px] rounded-full bg-[#008542]" />
                 Ertrag
               </div>
             </div>
